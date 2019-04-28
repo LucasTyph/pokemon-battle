43bae6b5310fb18d4e26e3908ff7a57622f3a76c
Vinicius <Vinicius@DESKTOP-FJNLQ4U>
2019-04-28 15:22:18 -0300

    teste

--- src/pokemonpack/Zapdos145.java
+++ src/pokemonpack/Zapdos145.java
@@ -2,6 +2,11 @@ package pokemonpack;
 
 public class Zapdos145 extends Pokemon{
 	public Zapdos145() {
-		super(364, 306, 295, 328, 145);
+		super(364, 306, 295, 328, 145, "Zapdos");
+		Attack a[] = new Attack[5];
+		a[1] = new Thunder();
+		a[2] = new SkyAttack();
+		a[3] = new HyperBeam();
+		a[4] = new ZapCannon();
 	}
 }
